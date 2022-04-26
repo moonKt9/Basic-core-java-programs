651439ed350bdc3503e7c05e2be5a5d2e34a2d64
Kritika <102981807+moonKt9@users.noreply.github.com>
2022-04-26 16:39:34 +0530

    Update Factorss.java

--- Factorss.java
+++ Factorss.java
@@ -1,10 +1,21 @@
 package com.bridgelabz.coreprogram;
-
-public class Factorss {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.util.*;
+public class Factorss{
+    public static void primeFactors(int n)
+    {
+        int c = 2;
+        while (n > 1) {
+            if (n % c == 0) {
+                System.out.print(c + " ");
+                n /= c;
+            }
+            else
+                c++;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int n = 315;
+        primeFactors(n);
+    }
 }
