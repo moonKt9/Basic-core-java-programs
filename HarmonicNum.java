df78e35480c4338476db6375c91094ac4d628958
Kritika <102981807+moonKt9@users.noreply.github.com>
2022-04-26 16:40:45 +0530

    Update HarmonicNum.java

--- HarmonicNum.java
+++ HarmonicNum.java
@@ -1,10 +1,22 @@
 package com.bridgelabz.coreprogram;
 
-public class HarmonicNum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.util.Scanner;
+ public class HarmonicNum
+{
+public static void main(String [] args)
+    {
+        System.out.print("Enter any number : ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.print("The Harmonic Series is : ");
+        double result = 0.0;
+        while(num > 0)
+          {
+               result = result + (double) 1 / num;
+               num--;
+               System.out.print(result +"  ");
+          }
+        System.out.println("");
+        System.out.println("Output of Harmonic Series is "+result);
+    }
 }
